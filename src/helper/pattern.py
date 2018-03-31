f7408ba3976e29a0ef834a82af4742fecb04b929
Unazed Spectaculum <leethax0r.coder@gmail.com>
2018-03-31 22:32:52 +0000

    pep8'd

--- src/helper/pattern.py
+++ src/helper/pattern.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python3
 import itertools
 
-bit_strings = itertools.chain(
-    range(0x78, 0x7f),
-    range(0x40, 0x46),
-    range(0x48, 0x4e),
-    range(0x50, 0x56),
-    range(0x58, 0x5e),
-    range(0x60, 0x66),
-    range(0x68, 0x6e),
-)
 
 def find_mask_and_pattern(bit_strings):
-    pattern_ones = ~0
+    pattern_ones = -1
     pattern_zeros = 0
 
     for bit_string in bit_strings:
@@ -24,6 +15,17 @@ def find_mask_and_pattern(bit_strings):
 
     return mask, pattern
 
-if __name__ == '__main__':
+
+bit_strings = itertools.chain(
+    range(0x78, 0x7f),
+    range(0x40, 0x46),
+    range(0x48, 0x4e),
+    range(0x50, 0x56),
+    range(0x58, 0x5e),
+    range(0x60, 0x66),
+    range(0x68, 0x6e),
+)
+
+if __name__ == "__main__":
     mask, pattern = find_mask_and_pattern(bit_strings)
     print("mask = {:08b}\tpattern = {:08b}".format(mask & 0xff, pattern & 0xff))
